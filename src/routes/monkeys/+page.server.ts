8a07726600486aca369de723ea564535d87afb14
Jerome Pinguet <jerome@jerome.cc>
2025-07-31 16:19:09 +0200

    WIP

---
+++ src/routes/monkeys/+page.server.ts
@@ -0,0 +1,36 @@
+import type { Actions, PageServerLoad } from './$types';
+import { variables } from '$lib/utils/constants';
+
+export const actions = {
+    default: async ({ request, fetch, cookies }) => {
+        const formData = await request.formData();
+        await fetch(`${variables.BASE_URI}/api/v2/auth/`);
+        console.log(`all cookies:${JSON.stringify(cookies.getAll())}`);
+        console.log(formData.get('name'))
+        const url = `${variables.BASE_URI}/api/v2/monkeys/`;
+        console.log(url);
+        const res = await fetch(url,
+            {
+                method: 'POST',
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify({'name': formData.get('name')})
+            }
+        );
+        if (res.ok) {
+            return {
+                success: true,
+                data: await res.json()
+            }
+        } else {
+            console.error(JSON.stringify(res))
+            return { success: false }
+        }
+    }
+} satisfies Actions;
+
+
+export const load: PageServerLoad = async ({ cookies }) => {
+    const res = await fetch(`${variables.BASE_URI}/api/v2/monkeys`);
+    const monkeys = await res.json();
+    return { monkeys };
+};
