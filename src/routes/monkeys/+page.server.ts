3ca2a36c4ee999b503ebdd58aacc7ee162c88ebe
Jerome Pinguet <jerome@jerome.cc>
2025-07-31 22:03:26 +0200

    WIP

--- src/routes/monkeys/+page.server.ts
+++ src/routes/monkeys/+page.server.ts
@@ -2,15 +2,15 @@ import type { Actions, PageServerLoad } from './$types';
 import { variables } from '$lib/utils/constants';
 
 export const actions = {
-    default: async ({ request, cookies }) => {
+    default: async ({ request, cookies, fetch }) => {
         const formData = await request.formData();
         const jwtres = await fetch(
-            `${variables.BASE_URI}/api/v2/jwt/`,
+            `${variables.BASE_URI}/api/v2/jwt`,
         {credentials: 'include'}
         );
         console.log(jwtres);
         const authres = await fetch(
-            `${variables.BASE_URI}/api/v2/auth/`,
+            `${variables.BASE_URI}/api/v2/auth`,
         {credentials: 'include'}
         );
         console.log(authres);
