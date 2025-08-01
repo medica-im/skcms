8233eb08d9b4e34fe66dea7e7d17b58bfb3fe6dc
Jerome Pinguet <jerome@jerome.cc>
2025-08-01 05:56:33 +0200

    wip

--- src/routes/monkeys/+page.server.ts
+++ src/routes/monkeys/+page.server.ts
@@ -1,49 +1,34 @@
 import type { Actions, PageServerLoad } from './$types';
 import { variables } from '$lib/utils/constants';
 
-const setAuthHeader = (request: Request, cookies: Array<{ name: string, value: string }>) => {
+const setAuthHeader = (request: Request, cookies: Array<{ name: string, value: string }>): Request => {
     request.headers.set(
         'cookie',
-        cookies.filter(({ name }) => ['authjs.session-token', '__Secure-authjs.session-token'].includes(name)).map(({ name, value }) => `${name}=${encodeURIComponent(value)}`).join('; ')
+        cookies.filter(({ value }) => value !== '').filter(({ name }) => ['authjs.session-token', '__Secure-authjs.session-token'].includes(name)).map(({ name, value }) => `${name}=${encodeURIComponent(value)}`).join('; ')
     )
+    return request
 }
 
 export const actions = {
     default: async (event) => {
         const formData = await event.request.formData();
-        let _request = new Request(
-            `${variables.BASE_URI}/api/v2/jwt`,
-            { credentials: 'include' }
-        );
-        _request.headers.set(
-            'cookie',
-            event.cookies
-                .getAll()
-                .filter(({ value }) => value !== '') // account for cookie that got deleted in the current request
-                .map(({ name, value }) => `${name}=${encodeURIComponent(value)}`)
-                .join('; ')
-        );
-        const jwtres = await fetch(_request);
-        let data = await jwtres.json();
-        console.log(data);
-        _request = new Request(
-            `${variables.BASE_URI}/api/v2/auth`, {
-            credentials: 'include'
-        });
-        setAuthHeader(_request, event.cookies.getAll());
-        const authres = await fetch(_request);
-        data = await authres.json();
-        console.log(data);
-        console.log(`all cookies:${JSON.stringify(event.cookies.getAll())}`);
-        console.log(formData.get('name'))
         const url = `${variables.BASE_URI}/api/v2/monkeys/`;
-        const res = await fetch(url,
-            {
+        let request = new Request(url,
+            {   credentials: 'include',
                 method: 'POST',
                 headers: { "content-type": "application/json" },
                 body: JSON.stringify({ 'name': formData.get('name') })
             }
         );
+        request.headers.set(
+            'cookie',
+            event.cookies
+                .getAll()
+                .filter(({ value }) => value !== '')
+                .map(({ name, value }) => `${name}=${encodeURIComponent(value)}`)
+                .join('; ')
+        );
+        const res = await fetch(request)
         if (res.ok) {
             return {
                 success: true,
@@ -57,7 +42,7 @@ export const actions = {
 } satisfies Actions;
 
 
-export const load: PageServerLoad = async ({ cookies }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
     const res = await fetch(`${variables.BASE_URI}/api/v2/monkeys`);
     const monkeys = await res.json();
     return { monkeys };
