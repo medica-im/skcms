c93c9cda6856561816e46c060b5a2cfbbfdfcb88
Jerome Pinguet <jerome@jerome.cc>
2025-12-03 14:33:51 +0100

    fix directory

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,34 +3,35 @@ import type { User } from "$lib/interfaces/user.interface";
 import type { Organization } from '$lib/interfaces/organization.ts';
 import type { LayoutServerLoad } from "./$types"
 
-export const load: LayoutServerLoad = async ({locals, cookies, fetch}) => {
-          let request = new Request(`${variables.BASE_URI}/api/v2/users/me`,
-              {   credentials: 'include',
-                  method: 'GET',
-                  headers: { "content-type": "application/json" },
-              }
-          );
-          request.headers.set(
-              'cookie',
-              cookies
-                  .getAll()
-                  .filter(({ value }) => value !== '')
-                  .map(({ name, value }) => `${name}=${encodeURIComponent(value)}`)
-                  .join('; ')
-          );
+export const load: LayoutServerLoad = async ({ locals, cookies, fetch }) => {
+  let request = new Request(`${variables.BASE_URI}/api/v2/users/me`,
+    {
+      credentials: 'include',
+      method: 'GET',
+      headers: { "content-type": "application/json" },
+    }
+  );
+  request.headers.set(
+    'cookie',
+    cookies
+      .getAll()
+      .filter(({ value }) => value !== '')
+      .map(({ name, value }) => `${name}=${encodeURIComponent(value)}`)
+      .join('; ')
+  );
   let response;
-  let user: User|undefined;
+  let user: User | undefined;
   try {
-  response = await fetch(request)
-  } catch (error)  {
-  console.log('There was an error', error);
+    response = await fetch(request)
+  } catch (error) {
+    console.log('There was an error', error);
   }
   if (response?.ok) {
     user = await response.json();
-  //console.log(`Use the response here: ${JSON.stringify(user)}`);
-} else {
-  console.log(`/api/v2/users/me HTTP Response Code: ${response?.status}`)
-}
+    //console.log(`Use the response here: ${JSON.stringify(user)}`);
+  } else {
+    console.log(`/api/v2/users/me HTTP Response Code: ${response?.status}`)
+  }
   let organization;
   response = await fetch(`${variables.BASE_URI}/api/v1/organization`)
   if (response.ok) {
@@ -38,10 +39,18 @@ export const load: LayoutServerLoad = async ({locals, cookies, fetch}) => {
   } else {
     console.error(`organization fetch HTTP Response Code: ${response?.status}`)
   }
+  let directory;
+  response = await fetch(`${variables.BASE_URI}/api/v1/directory`)
+  if (response.ok) {
+    directory = await response.json();
+  } else {
+    console.error(`directory fetch HTTP Response Code: ${response?.status}`)
+  }
 
   return {
     session: await locals.auth(),
     user: user,
-    organization: organization
+    organization: organization,
+    directory: directory
   }
 }
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,25 +2,11 @@ import { variables } from "$lib/utils/constants";
 import { checkVersion } from '$lib/version';
 import type { LayoutLoad } from './$types';
 
-const directory = async (fetch: any) => {
-  const url = `${variables.BASE_URI}/api/v1/directory/`;
-  let response;
-  try {
-    response = await fetch(url)
-  } catch (error) {
-    console.log('directory fetch', error);
-  }
-  if (response?.ok) {
-    return await response.json();
-  } else {
-    console.log(`directory fetch HTTP Response Code: ${response?.status}`)
-  }
-}
 
 export const load: LayoutLoad = async ({ fetch, parent, data }) => {
   checkVersion();
   return {
-    directory: await directory(fetch),
+    directory: data.directory,
     session: data.session,
     user: data.user,
     organization: data.organization,
