a5434c74b3664858e617cd0f1cdde585712eaa11
Jerome Pinguet <jerome@jerome.cc>
2025-12-11 11:04:25 +0100

    wip

---
+++ src/lib/components/Directory/SelectCategoriesR.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+	import Select from 'svelte-select';
+	import { onMount } from 'svelte';
+	import * as m from "$msgs";
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	import {
+		getSelectCategories,
+		getSelCatVal,
+		getDirectoryRedirect,
+		getSelectFacility,
+	} from '$lib/components/Directory/context';
+	import { getItems } from '$lib/components/Directory/SelectCategory.ts';
+	import type { Type } from '$lib/store/directoryStoreInterface';
+
+	let { categoryOf }:{ categoryOf: Promise<Type[]>} = $props();
+
+	const label = 'label';
+	const itemId = 'value';
+	let srcElement = $state();
+
+	let selCatVal = getSelCatVal();
+	let selectCategories = getSelectCategories();
+	let selectFacility = getSelectFacility();
+	let directoryRedirect = getDirectoryRedirect();
+	let filterText: string = $state("");
+	const itemFilter = () => true; // turn off internal filter
+
+	onMount(async () => {
+		const typesParam: string |null = page.url.searchParams.get('types');
+		if (typesParam != null) {
+		    const types: string[] = JSON.parse(typesParam);
+			$selectCategories=types;
+			const _categoryOf = await categoryOf;
+			const effector_type = _categoryOf.find(e=>types.includes(e.uid));
+			if ( effector_type ) {
+				$selCatVal = {value: effector_type.uid, label: effector_type.label||effector_type.name};
+			}
+		}
+	});
+
+	function getValue(selectCategories: string[], categories: Type[]) {
+		if (categories) {
+			let val = categories
+				.filter((x) => selectCategories.includes(x.uid))
+				.map(function (x) {
+					let dct = { value: x.uid, label: x.name };
+					return dct;
+				})[0];
+			return val;
+		}
+	};
+
+	function handleFocus(e: CustomEvent) {
+		srcElement=e.detail.srcElement;
+    };
+
+	const handleClear = async (event: CustomEvent) => {
+		if (event.detail) {
+			if (srcElement) srcElement.blur();
+			$selectCategories=[];
+			if (page.url.searchParams.has('types')) {
+				page.url.searchParams.delete('types');
+		    	await goto(page.url.pathname+"?"+page.url.searchParams);
+			}
+		    if (page.url.pathname != '/annuaire' && $directoryRedirect) {
+				let url = '/annuaire';
+				if ( $selectFacility ) {
+					url += `?facility=${$selectFacility}`
+				}
+			    await goto(url);
+		    }
+		}
+		if (srcElement) srcElement.blur();
+	};
+
+	function handleChange(event: CustomEvent) {
+		if (event.detail && event.detail.value) {
+			selectCategories.set([event.detail.value]);
+			if (srcElement) srcElement.blur();
+			const typesParam = JSON.stringify(`[${event.detail.value}]`)
+			let urlParams = `?types=${typesParam}`;
+			if ( $selectFacility ) {
+                urlParams += `&facility=${$selectFacility}`
+			}
+			if (page.url.pathname != '/annuaire' && $directoryRedirect) {
+				goto(`/annuaire${urlParams}`);
+			}
+		} else {
+			if (srcElement) srcElement.blur();
+		}
+	};
+</script>
+<!--srcElement: {srcElement}<br>
+$selectCategories: {JSON.stringify($selectCategories)}<br>
+$selCatVal: {JSON.stringify($selCatVal)}<br>
+categoryOf: {$categoryOf} ({$categoryOf.length})<br />
+filterText: {filterText}-->
+{#await categoryOf}
+	<div class="text-surface-700 theme">
+		<Select loading={true} placeholder={m.ADDRESSBOOK_CATEGORIES_PLACEHOLDER()} />
+	</div>
+{:then _categoryOf}
+	<div class="text-surface-700 z-auto theme">
+		<Select
+			{itemFilter}
+			{label}
+			{itemId}
+			items={getItems(_categoryOf, filterText)}
+			searchable={true}
+			on:focus={handleFocus}
+			on:change={handleChange}
+			on:clear={handleClear}
+			bind:value={$selCatVal}
+			bind:filterText
+			placeholder={m.ADDRESSBOOK_CATEGORIES_PLACEHOLDER()}
+		/>
+	</div>
+{/await}
+
+<style>
+	/*
+			CSS variables can be used to control theming.
+			https://github.com/rob-balfre/svelte-select/blob/master/docs/theming_variables.md
+	*/
+	.theme {
+		--border-radius: var(--theme-rounded-container);
+		--border-color: rgb(var(--color-secondary-500));
+		--border-focused: 1px solid rgb(var(--color-secondary-500));
+		--border-hover: 1px solid rgb(var(--color-secondary-500));
+		--item-active-outline: 1px solid rgb(var(--color-secondary-500));
+		--item-outline: 1px solid rgb(var(--color-secondary-500));
+		--clear-select-focus-outline: 1px solid rgb(var(--color-secondary-500));
+		--height: 3rem;
+	}
+</style>
---
+++ src/lib/components/Directory/SelectCommunesR.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import Select from 'svelte-select';
+	import { onMount } from 'svelte';
+	import * as m from '$msgs';
+	import { getSelectedCommunesUids, getSelectedCommunesChoices } from './context';
+	import type { Commune } from '$lib/interfaces/geography.interface.ts';
+	import type { SelectType } from '$lib/interfaces/select';
+
+	let { communeOf }: { communeOf: Promise<Commune[]> } = $props();
+
+	let multiple: boolean = $state(false);
+	const label = 'label';
+	const itemId = 'value';
+
+	let communesParam: string | null = null;
+
+	let selectedCommunes = getSelectedCommunesUids();
+	let selectedCommunesChoices = getSelectedCommunesChoices();
+	let value: SelectType | SelectType[] | undefined = $derived.by(() => {
+		if (multiple) {
+			return $selectedCommunesChoices || undefined;
+		} else {
+			return $selectedCommunesChoices ? $selectedCommunesChoices[0] : undefined;
+		}
+	});
+
+	onMount(async () => {
+		communesParam = page.url.searchParams.get('communes');
+		if (!communesParam) return;
+		const communeUids: string[] = JSON.parse(communesParam);
+		selectedCommunes.set(communeUids);
+		const cOf = await communeOf;
+		$selectedCommunesChoices = getChoices(communeUids, cOf);
+	});
+
+	function getItems(communes: Commune[]) {
+		return communes.map(function (x) {
+			let dct = { value: x.uid, label: x.name };
+			return dct;
+		});
+	}
+
+	function getChoices(communeUids: string[], allCommunes: Commune[]) {
+		const choices = allCommunes
+			.filter((x) => communeUids.includes(x.uid))
+			.map(function (x) {
+				let dct = { value: x.uid, label: x.name };
+				return dct;
+			});
+		return choices;
+	}
+
+	function handleClear(event: CustomEvent) {
+		if (event.detail) {
+			$selectedCommunes = [];
+			$selectedCommunesChoices = null;
+			if (page.url.searchParams.get('communes')) {
+				page.url.searchParams.delete('communes');
+				goto(page.url.pathname + '?' + page.url.searchParams);
+			}
+		}
+	}
+
+	function handleChange(event: CustomEvent) {
+		if (event.detail) {
+			if (Array.isArray(event.detail)) {
+				$selectedCommunesChoices = event.detail;
+				$selectedCommunes = event.detail.map((e) => e.value);
+			} else {
+				$selectedCommunesChoices = [event.detail];
+				$selectedCommunes = [event.detail.value];
+			}
+		}
+	}
+</script>
+<!--
+{communeOf ? JSON.stringify(communeOf.slice(0, 2)) : communeOf}<br />
+{communeOf ? communeOf?.length : 0} commune(s)<br />
+$selectedCommunes: {JSON.stringify($selectedCommunes)}<br />
+typeof $selectedCommunes: {typeof $selectedCommunes}<br />
+$selectedCommunesChoices: {JSON.stringify($selectedCommunesChoices)}
+-->
+<div class="text-surface-700 theme max-h-12">
+	{#await communeOf}
+		<Select loading={true} placeholder={m.ADDRESSBOOK_COMMUNES_PLACEHOLDER()} />
+	{:then cOf}
+		<Select
+			{label}
+			{itemId}
+			items={getItems(cOf)}
+			searchable={true}
+			on:change={handleChange}
+			on:clear={handleClear}
+			placeholder={m.ADDRESSBOOK_COMMUNES_PLACEHOLDER()}
+			{multiple}
+			bind:value
+		/>
+	{/await}
+</div>
+
+<style>
+	/*
+			CSS variables can be used to control theming.
+			https://github.com/rob-balfre/svelte-select/blob/master/docs/theming_variables.md
+	*/
+	.theme {
+		--border-radius: var(--theme-rounded-container);
+		--border-color: rgb(var(--color-secondary-500));
+		--border-focused: 1px solid rgb(var(--color-secondary-500));
+		--border-hover: 1px solid rgb(var(--color-secondary-500));
+		--item-active-outline: 1px solid rgb(var(--color-secondary-500));
+		--item-outline: 1px solid rgb(var(--color-secondary-500));
+		--clear-select-focus-outline: 1px solid rgb(var(--color-secondary-500));
+		--height: 3rem;
+	}
+</style>
