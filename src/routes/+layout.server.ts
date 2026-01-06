b9c4af243560f44c26e4c42d4cc40ef995e0c603
Jerome Pinguet <jerome@jerome.cc>
2026-01-06 18:04:22 +0100

    retrieve everything from the server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -7,37 +7,32 @@ import type { Entry } from '$lib/store/directoryStoreInterface';
 
 export const load: LayoutServerLoad = async ({ locals, cookies, fetch }) => {
   let response;
-  let user: User|undefined;
-  if (import.meta.env.DEV) {
-    const userUrl = `${ORIGIN}/api/v2/users/me`;
-    const request = authReq(userUrl, "GET", cookies);
-    try {
-      response = await fetch(request);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      user = await response.json();
-      console.log("user layout.server.ts", user);
-    } catch (error: any) {
-      console.error('There was an error while retrieving user from layout.server.ts', error.message);
+  let user: User | undefined;
+  const userUrl = `${ORIGIN}/api/v2/users/me`;
+  const request = authReq(userUrl, "GET", cookies);
+  try {
+    response = await fetch(request);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+    user = await response.json();
+    console.log("user layout.server.ts", user);
+  } catch (error: any) {
+    console.error('There was an error while retrieving user from layout.server.ts', error.message);
   }
   // here retrieving entries with auth works with http://localhost and pnpm run dev
-    let entries: Entry[]|undefined;
-      if (import.meta.env.DEV) {
-
-    const entriesUrl = `${ORIGIN}/api/v2/entries`;
-    const entriesRequest = authReq(entriesUrl, "GET", cookies);
-    try {
-      response = await fetch(entriesRequest);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      entries = await response.json();
-      if (entries) console.log('entries layout.server.ts', entries[0]);
-    } catch (error: any) {
-      console.error('There was an error while retrieving entries from layout.server.ts', error.message);
+  let entries: Entry[] | undefined;
+  const entriesUrl = `${ORIGIN}/api/v2/entries`;
+  const entriesRequest = authReq(entriesUrl, "GET", cookies);
+  try {
+    response = await fetch(entriesRequest);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+    entries = await response.json();
+    if (entries) console.log('entries layout.server.ts', entries[0]);
+  } catch (error: any) {
+    console.error('There was an error while retrieving entries from layout.server.ts', error.message);
   };
   let organization;
   const orgUrl = `${ORIGIN}/api/v2/organization`;
