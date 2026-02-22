e86bdf0daefcbec3cea31f9e9a5d4ff3ffe56bd0
Jerome Pinguet <jerome@jerome.cc>
2026-02-22 15:25:34 +0100

    complete matcher

--- src/params/facility.ts
+++ src/params/facility.ts
@@ -1,7 +1,7 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
 // Excludes 'web' and other reserved paths from being matched as a facility
-const reserved = ['web', 'api', 'signin', 'signout', 'dashboard', 'contact', 'sites', 'test', 'ghost'];
+const reserved = ['web', 'api', 'signin', 'signout', 'dashboard', 'contact', 'sites', 'test', 'ghost', 'mentions-legales'];
 
 export const match: ParamMatcher = (param) => {
     return !reserved.includes(param);
