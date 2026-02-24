e69b05ea0b946b0ec43d3842c8f31d8277568a02
Jerome Pinguet <jerome@jerome.cc>
2026-02-25 00:23:30 +0100

    use globalThis.fetch to avoid cookie tampering by sveltekit fetch

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -12,11 +12,10 @@ export const load: LayoutServerLoad = async ({ locals, cookies, fetch, depends }
   depends('app:entries');
   let response;
   let user: User | undefined;
-  if ( import.meta.env.DEV ) {
     const userUrl = `${ORIGIN}/api/v2/users/me`;
     const request = authReq(userUrl, "GET", cookies);
     try {
-      response = await fetch(request);
+      response = await globalThis.fetch(request);
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
@@ -25,21 +24,9 @@ export const load: LayoutServerLoad = async ({ locals, cookies, fetch, depends }
     } catch (error: any) {
       console.error('There was an error while retrieving user from layout.server.ts', error.message);
     }
-  }
   // here retrieving entries with auth works with http://localhost and pnpm run dev
-  let entries: Entry[] | undefined = await getEntries(fetch, cookies);
-    /*const entriesUrl = `${ORIGIN}/api/v2/entries`;
-    const entriesRequest = authReq(entriesUrl, "GET", cookies);
-    try {
-      response = await fetch(entriesRequest);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      entries = await response.json() as Entry[];
-      if (entries) console.log('entries layout.server.ts', entries[0].name);
-    } catch (error: any) {
-      console.error('There was an error while retrieving entries from layout.server.ts', error.message);
-    };*/
+  let entries: Entry[] | undefined = await getEntries(globalThis.fetch, cookies);
+
   let organization;
   const orgUrl = `${ORIGIN}/api/v2/organization`;
   try {
@@ -55,6 +42,7 @@ export const load: LayoutServerLoad = async ({ locals, cookies, fetch, depends }
   } catch (error: any) {
     console.error(`organization from layout.server.ts ${error.message}`);
   }
+
   let directory;
   const dirUrl = `${ORIGIN}/api/v1/directory/`;
   try {
