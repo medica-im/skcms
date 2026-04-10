f473a2293295ee81c6429ed382dc2a46399657ab
Jerome Pinguet <jerome@jerome.cc>
2026-04-10 11:49:47 +0200

    use new url scheme for entries

--- src/lib/components/Directory/Entries.svelte
+++ src/lib/components/Directory/Entries.svelte
@@ -3,19 +3,17 @@
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import { faUser } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
-	import { entryUrl } from '$lib/utils/utils.ts';
+	import { entrySlugPageUrl } from '$lib/utils/utils.ts';
 	import type { Entry } from '$lib/store/directoryStoreInterface';
 
 	export let data: Entry[];
 
 	const pathname = page.url.pathname;
-	const org = page.data.organization.category.name;
-	
 </script>
 
 <div class="flex flex-wrap gap-2 p-2">
 	{#each data as entry}
-		<a href={entryUrl(entry, pathname, org, true)} class="btn btn-sm variant-ghost gap-1 p-1">
+		<a href={entrySlugPageUrl(entry, pathname)} class="btn btn-sm variant-ghost gap-1 p-1">
 			<Avatar src={entry?.avatar?.sm||entry?.avatar?.lg||entry?.avatar?.raw} width="w-10 lg:w-12">
 				<Fa icon={faUser} />
 			</Avatar>
