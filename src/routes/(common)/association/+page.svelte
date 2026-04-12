753a76dd615d998e77b0c6fb393c2ad85be55c7f
Jerome Pinguet <jerome@jerome.cc>
2026-04-12 20:44:05 +0200

    association page UX

--- src/routes/(common)/association/+page.svelte
+++ src/routes/(common)/association/+page.svelte
@@ -11,17 +11,15 @@
 	let tabIndex: number = $state(0);
 
 	const entries: Entry[] = $derived(page.data?.entries || []);
-	const activeOfficers = $derived(
-		(data.officers || []).filter((o) => !o.stop)
-	);
-	const activeBoardMembers = $derived(
-		(data.boardMembers || []).filter((m) => !m.stop)
-	);
+	const activeOfficers = $derived((data.officers || []).filter((o) => !o.stop));
+	const activeBoardMembers = $derived((data.boardMembers || []).filter((m) => !m.stop));
 </script>
 
-<div class="w-full max-w-5xl mx-auto p-4 space-y-6">
-	<h2 class="h2">{m.ASSOCIATION_TITLE()}</h2>
+<svelte:head>
+	<title>{m.ASSOCIATION_TITLE()}</title>
+</svelte:head>
 
+<div class="w-full max-w-5xl mx-auto p-4 space-y-6">
 	<TabGroup>
 		<Tab bind:group={tabIndex} name="officers" value={0}>
 			{m.OFFICERS_SHORT()}
@@ -31,25 +29,34 @@
 		</Tab>
 		<svelte:fragment slot="panel">
 			{#if tabIndex === 0}
-				<div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
-					{#each activeOfficers as officer (officer.uid)}
-						<OfficerCard
-							{officer}
-							{entries}
-							organizationRoles={data.organizationRoles || []}
-							organizationRoleLabels={data.organizationRoleLabels || {}}
-						/>
-					{/each}
+				<div class="space-y-6 lg:space-y-8">
+					<h2 class="h2">{m.ASSOCIATION_TITLE()} > {m.OFFICERS()}</h2>
+
+					<div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+						{#each activeOfficers as officer (officer.uid)}
+							<OfficerCard
+								{officer}
+								{entries}
+								organizationRoles={data.organizationRoles || []}
+								organizationRoleLabels={data.organizationRoleLabels || {}}
+							/>
+						{/each}
+					</div>
 				</div>
 				{#if !activeOfficers.length}
 					<p class="text-center opacity-60">{m.NO_OFFICER()}</p>
 				{/if}
 			{:else if tabIndex === 1}
+							<div class="space-y-6 lg:space-y-8">
+
+				<h2 class="h2">{m.ASSOCIATION_TITLE()} > {m.BOARD_MEMBERS()}</h2>
+
 				<BoardMembersDisplay
 					boardMembers={activeBoardMembers}
 					membershipCategories={data.membershipCategories || []}
 					{entries}
 				/>
+				</div>
 			{/if}
 		</svelte:fragment>
 	</TabGroup>
