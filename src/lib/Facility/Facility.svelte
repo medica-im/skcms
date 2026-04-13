cae54daf2944c3e1668b6c479e4b4ee50b73e766
Jerome Pinguet <jerome@jerome.cc>
2026-04-13 15:26:04 +0200

    improve Facility carousel UX with image switch

--- src/lib/Facility/Facility.svelte
+++ src/lib/Facility/Facility.svelte
@@ -8,8 +8,9 @@
 	import type { Facility } from '$lib/interfaces/facility.interface.js';
 	import { JsonView } from '@zerodevx/svelte-json-view';
 
-	let { data }: { data: Facility[] } = $props();
+	let { data, carousel=true }: { data: Facility[]; carousel?: boolean } = $props();
 
+	const lgCols = carousel ? 3 : 2;
 	function filterFacilities(facilities: Facility[]) {
 		const f = facilities.filter((facility) =>
 			facility.avatar !== null
@@ -24,35 +25,33 @@
 	}
 </script>
 
-<div class="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-10">
+<div class="grid grid-cols-1 lg:grid-cols-{lgCols} gap-6 lg:gap-10 items-start">
 	<div class="lg:col-span-3 text-center">
 		<h2 class="h2">{capitalizeFirstLetter(m.SITE_COUNT({ count: data.length }))}</h2>
 	<p>{m.OUR_FACILITIES({ count: data.length })}</p>
 	</div>
 
-	<div class="space-y-4 text-center">
+	<div class="flex flex-wrap items-center gap-4 text-center">
 		{#each data.sort(compareFn) as facility}
-			<div>
 				<a
 					href="/sites/{facility.slug||facility.uid}"
 					title={facility.name}
-					class="btn variant-ghost-primary w-fit">{facility.label || facility.name}</a
+					class="btn btn-sm variant-ghost-primary w-fit">{facility.label || facility.name}</a
 				>
-			</div>
 		{/each}
 	</div>
-	<div in:scale class="h-64 z-0 self-center">
+	<div in:scale class="h-64 z-0">
 		<Map data={createFacilitiesMapData(data, true)} showTooltip={true} />
 	</div>
-	{#if carouselFacilities.length}
+	{#if carouselFacilities.length && carousel}
 		<div class="place-items-center items-center justify-center content-center">
 			<FacilityCarousel data={data.filter((f: Facility)=>{return f.avatar!==null})} />
 		</div>
 	{/if}
 </div>
-{#if import.meta.env.DEV}
+<!--{#if import.meta.env.DEV}
 	<details class="p-2">
 		<summary class="cursor-pointer text-sm text-surface-500">JSON</summary>
 		<JsonView json={data} depth={1} />
 	</details>
-{/if}
+{/if}-->
