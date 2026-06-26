e664db7662f09279fe73b4a1c8561747cd02e67d
Jerome Pinguet <jerome@jerome.cc>
2026-06-26 18:37:19 +0200

    fix mixed content

--- src/lib/utils/origin.ts
+++ src/lib/utils/origin.ts
@@ -1,4 +1,4 @@
 import { PUBLIC_ORIGIN } from '$env/static/public';
 import { browser } from '$app/environment';
 
-export const ORIGIN = (import.meta.env.DEV && browser) ? '' : PUBLIC_ORIGIN;
+export const ORIGIN = browser ? '' : PUBLIC_ORIGIN;
